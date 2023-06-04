3aaa9318cce99ac3354bc0af093b437f7ed126a3
Jakub Ciupka <ciupkajakub@gmail.com>
2023-06-04 15:17:51 +0200

    Prepare active record encryption variables

--- config/application.rb
+++ config/application.rb
@@ -38,5 +38,10 @@ module Shop
     origins = ENV.fetch("ACTION_CABLE_ALLOWED_REQUEST_ORIGINS") { "http:\/\/localhost*" }.split(",")
     origins.map! { |url| /#{url}/ }
     config.action_cable.allowed_request_origins = origins
+
+    # set active record encryption
+    config.active_record.encryption.primary_key = ENV.fetch("ACTIVE_RECORD_ENCRYPTION_PRIMARY_KEY")
+    config.active_record.encryption.deterministic_key = ENV.fetch("ACTIVE_RECORD_ENCRYPTION_DETERMINISTIC_KEY")
+    config.active_record.encryption.key_derivation_salt = ENV.fetch("ACTIVE_RECORD_ENCRYPTION_KEY_DERIVATION_SALT")
   end
 end
