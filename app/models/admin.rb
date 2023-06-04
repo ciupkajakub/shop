a887f59a2f70660652223f88b7f42102793a774e
Jakub Ciupka <ciupkajakub@gmail.com>
2023-06-04 15:24:02 +0200

    Generate two-factor for devise model

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,5 +1,5 @@
 class Admin < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :recoverable, :rememberable, :validatable
+  devise :recoverable, :rememberable, :validatable, :two_factor_authenticatable
 end
