e3a603099de1feb2c85adeeddcf8a04ce06cc0fc
Jakub Ciupka <ciupkajakub@gmail.com>
2023-06-18 13:49:51 +0200

    Add Product migration

---
+++ db/migrate/20230618114911_create_products.rb
@@ -0,0 +1,11 @@
+class CreateProducts < ActiveRecord::Migration[7.0]
+  def change
+    create_table :products, id: :uuid do |t|
+      t.string :name
+      t.text :description
+      t.belongs_to :product_category, foreign_key: true, type: :uuid
+
+      t.timestamps
+    end
+  end
+end
