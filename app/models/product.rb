fc468d1af7d092c39a20e388dbc9239d393895c6
Jakub Ciupka <ciupkajakub@gmail.com>
2023-06-20 16:25:02 +0200

    Include the action text

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,14 @@
 class Product < ApplicationRecord
+  has_one_attached :image
+  has_many_attached :pictures
+  has_rich_text :description
+  belongs_to :product_category
+
+  def image_as_thumbnail
+    image.variant(resize_to_limit: [300, 300]).processed
+  end
+
+  def picture_as_thumbnail(picture)
+    picture.variant(resize_to_limit: [200, 200]).processed
+  end
 end
