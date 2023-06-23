613a34f0264cca4beaad94990a8abec75e5e34ae
Jakub Ciupka <ciupkajakub@gmail.com>
2023-06-23 15:24:15 +0200

    Run migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_23_124907) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_23_131041) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -96,6 +96,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_23_124907) do
     t.uuid "product_category_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "price"
     t.index ["product_category_id"], name: "index_products_on_product_category_id"
   end
 
