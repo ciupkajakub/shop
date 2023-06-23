b78d9dcc4ca526cd3a467ff144b6d2011f0f9a1a
Jakub Ciupka <ciupkajakub@gmail.com>
2023-06-23 14:53:20 +0200

    Add Cart model and related objects

---
+++ db/migrate/20230623124907_create_orderables.rb
@@ -0,0 +1,11 @@
+class CreateOrderables < ActiveRecord::Migration[7.0]
+  def change
+    create_table :orderables, id: :uuid do |t|
+      t.belongs_to :product, null: false, foreign_key: true, type: :uuid
+      t.belongs_to :cart, null: false, foreign_key: true, type: :uuid
+      t.integer :quantity
+
+      t.timestamps
+    end
+  end
+end
