c9b9b7c002beabce5a9ddc6b15cf443c3f1f6f91
Jakub Ciupka <ciupkajakub@gmail.com>
2023-06-27 14:23:10 +0200

    Add belong_to to the Cart

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,7 +1,8 @@
 class Cart < ApplicationRecord
   has_many :orderables
   has_many :products, through: :orderables
-
+  belongs_to :user, optional: true
+  
   def total
     orderables.to_a.sum(&:total)
   end
