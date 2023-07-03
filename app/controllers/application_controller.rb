1d3d18478ea44b4deb943dccc367b821d6d0ef30
Jakub Ciupka <ciupkajakub@gmail.com>
2023-07-03 18:34:49 +0200

    Create cart only when adding the first item

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,14 +8,5 @@ class ApplicationController < ActionController::Base
 
   def initialize_cart
     @cart ||= Cart.find_by(id: session[:cart_id])
-    # TODO: do not initialize empty cart every time, we need that only when someone adds a product to the cart
-    create_cart if @cart.nil?
-  end
-
-  private
-
-  def create_cart
-    @cart = Cart.create
-    session[:cart_id] = @cart.id
   end
 end
--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -4,9 +4,11 @@ class CartsController < ApplicationController
   end
 
   def add
+    cart = find_or_create_cart
+
     @product = Product.find_by(id: params[:id])
     quantity = params[:quantity].to_i
-    current_orderable = @cart.orderables.find_by(product_id: @product.id)
+    current_orderable = cart.orderables.find_by(product_id: @product.id)
 
     if current_orderable && quantity > 0
       current_orderable.update(quantity:)
@@ -19,5 +21,15 @@ class CartsController < ApplicationController
 
   def remove
     Orderable.find_by(id: params[:id]).destroy
+  end
+
+  private
+
+  def find_or_create_cart
+    @cart ||= Cart.find_by(id: session[:cart_id]) || Cart.create
+
+    session[:cart_id] = @cart.id
+
+    @cart
   end
 end
