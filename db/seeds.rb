a60fe9a558c0a98d66dfad3cff307f21768b1c78
Jakub Ciupka <ciupkajakub@gmail.com>
2023-08-06 21:19:11 +0200

    Update seeds https://github.com/ciupkajakub/shop/issues/2

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,3 +13,20 @@ _admin = Admin.create(
   otp_required_for_login: true,
   otp_secret: Admin.generate_otp_secret
 )
+
+clothing_category = ProductCategory.create(name: 'CLOTHING')
+men_product_category = ProductCategory.create(name: 'MEN', product_category_id: clothing_category.id)
+hoodie_category = ProductCategory.create(name: 'HOODIES', product_category_id: men_product_category.id)
+product = Product.create(name: 'Hoodie v100', description: '100% cotton', product_category_id: hoodie_category.id)
+product_item = ProductItem.create(
+  sku: 'random_string',
+  qty_in_stock: 10,
+  price: 20_000,
+  is_default: true,
+  product_id: product.id
+)
+product_item_config = ProductItemConfig.create(product_item_id: product_item.id)
+variant_1 = SizeVariant.create(name: 'SIZE', value: 'M', product_category_id: hoodie_category.id)
+variant_2 = ColorVariant.create(name: 'COLOR', value: 'BLACK', product_category_id: hoodie_category.id)
+
+product_item_config.variants << [variant_1, variant_2]
